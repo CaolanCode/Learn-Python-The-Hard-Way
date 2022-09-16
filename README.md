1fb513ea3aefd1d349da71e5a678660a99358246
CaolanCode <cmacmahon3@gmail.com>
2022-09-16 17:02:02 +0100

    initial commit of first three exercises

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn Python The Hard Way
+
+This repository will contain exercises from the book
---
+++ ex3.py
@@ -0,0 +1,26 @@
+print("I will now count my chickens!")
+
+# 30 / 6 = 5, 5 + 25 = 30
+print("Hens ", 25.0 + 30 / 6)
+# 25 * 3 = 75, 75 % 4 = 3, 100 - 3 =97
+print("Roosters ", 100.0 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# 1 / 4 = 0.25, 4 % 2 = 0, 3 + 2 + 1 - 5 + 0 - 0.25 + 6 = 6.75
+print(3 + 2 + 1 - 5.0 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+# 5
+print("What is 3 + 2?", 3.0 + 2)
+# -2
+print("What is 5 - 7?", 5.0 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+# true
+print("Is it greater?", 5.0 > -2)
+# true
+print("Is it greater?", 5.0 >= -2)
+# false
+print("Is it less or equal?", 5.0 <= -2)
