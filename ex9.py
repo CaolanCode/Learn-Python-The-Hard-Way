cbd60ec87c2e7fdc3a9bd2e19ce9a5739fb54c9f
CaolanCode <cmacmahon3@gmail.com>
2022-09-17 12:55:11 +0100

    update repo with files

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly
+
+days = "Mon Tues Wed Thurs Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("""
+There's something going on here
+      with the three double-qoutes.
+      We'll be able to type as much as we like.
+      Even four lines if we want, or five, or six.
+      """)
+
