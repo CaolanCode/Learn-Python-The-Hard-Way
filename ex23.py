bbb025005e16c7a5f893b38dc3feaabd6deb96b8
CaolanCode <cmacmahon3@gmail.com>
2022-09-17 19:17:24 +0100

    update repo with files

--- ex23.py
+++ ex23.py
@@ -2,16 +2,16 @@ import sys
 script, encoding, error = sys.argv
 
 def main(language_file, encoding, errors):
-    line = language_file.readline()
+    line = language_file.readline() # read one line from languages.txt
 
-    if line:
-        print_line(line, encoding, errors)
+    if line: # if line contains something
+        print_line(line, encoding, errors) 
         return main(language_file, encoding,errors)
 
 def print_line(line, encoding, errors):
     next_lang = line.strip() # remove leading and trailing bytes
-    raw_bytes = next_lang.encode(encoding, errors=errors)
-    cooked_string = raw_bytes.decode(encoding, errors=errors)
+    raw_bytes = next_lang.encode(encoding, errors=errors) # encode into raw bytes
+    cooked_string = raw_bytes.decode(encoding, errors=errors) # decode the raw bytes back to a string
     print(raw_bytes, "<===>", cooked_string)
 
 languages = open("languages.txt", encoding="utf=8")
